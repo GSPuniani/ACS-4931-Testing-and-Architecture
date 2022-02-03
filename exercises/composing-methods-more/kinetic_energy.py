941e4575c6ca5b8d28b7827d365d12f5b0a12bd0
Gobind Puniani <gobindpuniani@gmail.com>
2022-02-03 11:28:06 -0800

    Refactored composing-methods-more

--- exercises/composing-methods-more/kinetic_energy.py
+++ exercises/composing-methods-more/kinetic_energy.py
@@ -15,22 +15,24 @@ def calculate_kinetic_energy(mass, distance, time):
     if distance.unit != 'km':
         if distance.unit == "ly":  # [ly] stands for light-year (measure of distance in astronomy)
             # convert from light-year to km unit
-            in_km = distance.value * 9.461e12
-            distance = Distance(in_km, "km")
-        else:
-            print ("unit is Unknown")
-            return
-    speed = distance.value/time # [km per sec]
-    if mass.unit != 'kg':
-        if mass.unit == "solar-mass":
-            # convert from solar mass to kg
-            value = mass.value * 1.98892e30 # [kg]
-            mass = Mass(value, 'kg')
+            distance_value = distance.value * 9.461e12
+            adjusted_distance = Distance(distance_value, "km")
         else:
             print ("unit is Unknown")
             return
 
-    kinetic_energy = 0.5 * mass.value * speed ** 2
+    speed = adjusted_distance.value / time if adjusted_distance else distance.value / time # [km per sec]
+
+    if mass.unit != 'kg':
+        if mass.unit == "solar-mass":
+            # convert from solar mass to kg
+            mass_value = mass.value * 1.98892e30 # [kg]
+            adjusted_mass = Mass(mass_value, 'kg')
+        else:
+            print ("unit is Unknown")
+            return
+
+    kinetic_energy = 0.5 * adjusted_mass.value * speed ** 2 if adjusted_mass else 0.5 * mass.value * speed ** 2
     return kinetic_energy
 
 mass = Mass(2, "solar-mass")
